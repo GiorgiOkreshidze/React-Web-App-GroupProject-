3aed029bd1025362b656689916dc23084fe2ac30
DESKTOP-9TE5ILI\kiustudent <tsirekidze.nini@kiu.edu.ge>
2023-06-16 14:31:16 +0400

    .

--- src/Products.tsx
+++ src/Products.tsx
@@ -997,6 +997,134 @@ function Products() {
           </div>
         </div>
       ))}
+
+      <div className="bg-white border-radius-md-10 h-56px h-md-40px d-flex align-items-center justify-content-center mt-16px">
+        <ul className="pagination d-flex align-items-center list-unstyled">
+          <li>
+            {/* <span className="d-flex p-12px cursor-pointer">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="13.414"
+                height="8.829"
+                viewBox="0 0 13.414 8.829"
+              >
+                <g transform="translate(1 1.414)">
+                  <path
+                    d="M12,12,9,9l3-3"
+                    transform="translate(-1 -6)"
+                    style={{fill: 'none', stroke: 'rgb(253, 65, 0)', strokeLinecap: 'round', strokeWidth: '2px', strokeLinejoin: 'round',}}
+                  ></path>
+                  <path
+                    d="M12,12,9,9l3-3"
+                    transform="translate(-6 -6)"
+                    style={{fill: 'none', stroke: 'rgb(253, 65, 0)', strokeLinecap: 'round', strokeWidth: '2px', strokeLinejoin: 'round',}}
+                  ></path>
+                  <line
+                    y2="6"
+                    transform="translate(0)"
+                    style={{fill: 'none', stroke: 'rgb(253, 65, 0)', strokeLinecap: 'round', strokeWidth: '2px'}}
+                  ></line>
+                </g>
+              </svg>
+            </span> */}
+          </li>
+          <li>
+            {/* <span className="d-flex p-12px cursor-pointer">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="5.414"
+                height="8.829"
+                viewBox="0 0 5.414 8.829"
+              >
+                <path
+                  d="M12,12,9,9l3-3"
+                  transform="translate(-8 -4.586)"
+                  style={{fill: 'none', stroke: 'rgb(253, 65, 0)', strokeLinecap: 'round', strokeWidth: '2px', strokeLinejoin: 'round',}}
+                ></path>
+              </svg>
+            </span> */}
+          </li>
+          <li>
+            <span className="actived-flex p-12px text-gray-400 opacity-50 font-size-18 font-size-md-14 font-medium cursor-pointer active pointer-events-none">
+              1
+            </span>
+          </li>
+          <li>
+            <span className="actived-flex p-12px text-gray-400 opacity-50 font-size-18 font-size-md-14 font-medium cursor-pointer ">
+              2
+            </span>
+          </li>
+          <li>
+            <span className="actived-flex p-12px text-gray-400 opacity-50 font-size-18 font-size-md-14 font-medium cursor-pointer ">
+              3
+            </span>
+          </li>
+          <li>
+            <span className="actived-flex p-12px text-gray-400 opacity-50 font-size-18 font-size-md-14 font-medium cursor-pointer ">
+              4
+            </span>
+          </li>
+          <li>
+            <span className="actived-flex p-12px text-gray-400 opacity-50 font-size-18 font-size-md-14 font-medium cursor-pointer ">
+              5
+            </span>
+          </li>
+          <li>
+            <span className="actived-flex p-12px text-gray-400 opacity-50 font-size-18 font-size-md-14 font-medium cursor-pointer ">
+              6
+            </span>
+          </li>
+          <li>
+            <span className="actived-flex p-12px text-gray-400 opacity-50 font-size-18 font-size-md-14 font-medium cursor-pointer ">
+              7
+            </span>
+          </li>
+          <li>
+            <span className="d-flex p-12px cursor-pointer">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="5.414"
+                height="8.829"
+                viewBox="0 0 5.414 8.829"
+              >
+                <path
+                  d="M9,12l3-3L9,6"
+                  transform="translate(-7.586 -4.586)"
+                  style={{fill: 'none', stroke: 'rgb(253, 65, 0)', strokeLinecap: 'round', strokeWidth: '2px', strokeLinejoin: 'round',}}
+                ></path>
+              </svg>
+            </span>
+          </li>
+          <li>
+            <span className="d-flex p-12px cursor-pointer">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="13.414"
+                height="8.829"
+                viewBox="0 0 13.414 8.829"
+              >
+                <g transform="translate(-1134.586 -2682.586)">
+                  <path
+                    d="M9,12l3-3L9,6"
+                    transform="translate(1127 2678)"
+                    style={{fill: 'none', stroke: 'rgb(253, 65, 0)', strokeLinecap: 'round', strokeWidth: '2px', strokeLinejoin: 'round',}}
+                  ></path>
+                  <path
+                    d="M9,12l3-3L9,6"
+                    transform="translate(1132 2678)"
+                    style={{fill: 'none', stroke: 'rgb(253, 65, 0)', strokeLinecap: 'round', strokeWidth: '2px', strokeLinejoin: 'round',}}
+                  ></path>
+                  <line
+                    y2="6"
+                    transform="translate(1147 2684)"
+                    style={{fill: 'none', stroke: 'rgb(253, 65, 0)', strokeLinecap: 'round', strokeWidth: '2px', }}
+                  ></line>
+                </g>
+              </svg>
+            </span>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 }
